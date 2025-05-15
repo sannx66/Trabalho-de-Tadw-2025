82c02565fefe7d21c170c858bff0a8a2cad36be2
yomirkz <izaxxxsouza7777@gmail.com>
2025-05-15 08:51:09 -0300

    fiz as funções da entrega e editei um nome de carrinho pq o codigo tava dando erro devido ao nome repetido

--- tadw_e_dps_sanbenny/codigo/funcoes.php
+++ tadw_e_dps_sanbenny/codigo/funcoes.php
@@ -107,7 +107,7 @@ function salvarCarrinho($conexao, $produto, $quantidade, $valor_un, $valor_entre
 
 };
 
-function salvarCarrinho($conexao, $valor_entrega, $valor_total, $valor_pago, $troco, $data_hora, $idcliente) {};
+function oaarCarrinho($conexao, $valor_entrega, $valor_total, $valor_pago, $troco, $data_hora, $idcliente) {};
 function listarCarrinho($conexao) {};
 function listarItensCarrinho($conexao) {};
 function editarCarrinho($conexao, $valor_entrega, $valor_total, $valor_pago, $troco, $data_hora, $idcliente) {};
@@ -117,12 +117,71 @@ function pesquisarCarrinho($conexao, $nome) {};
 function calculoTotal ($conexao, $quantidade, $valor_un) {};
 function calculoEntrega ($conexao, $valor_total, $entrega) {};
 function calculoTroco ($valor_pago, $valor_total) {};
+//sandy
 
-function cadastrarEntrega($conexao, $entregador, $idcarrinho) {};
-function listarEntrega($conexao) {};
-function editarEntrega($conexao, $entregador, $idcarrinho) {};
-function deletarEntrega($conexao, $identrega) {};
-function pesquisarEntrega($conexao, $identrega) {};
+function salvarEntrega($conexao, $entregador, $idcarrinho) {
+    $sql = "INSERT INTO entrega (entregador, idcarrinho) VALUES (?, ?)";
+   $comando = mysqli_prepare($conexao, $sql);
+   
+   mysqli_stmt_bind_param($comando, 'si', $entregador, $idcarrinho);
+   
+   mysqli_stmt_execute($comando);
+   mysqli_stmt_close($comando);
+}
+
+function listarEntregas($conexao) {
+    $sql = "SELECT * FROM entrega";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($comando);
+    $resultados = mysqli_stmt_get_result($comando);
+
+    $lista_entregas = [];
+    while ($entrega = mysqli_fetch_assoc($resultados)) {
+        $lista_entregas[] = $entrega;
+    }
+    mysqli_stmt_close($comando);
+
+    return $lista_entregas;
+}
+
+function editarEntrega($conexao, $entregador, $idcarrinho, $identrega){
+    $sql = "UPDATE entrega SET entregador=?, idcarrinho=?WHERE identrega=?";
+    $comando = mysqli_prepare($conexao, $sql);
+    mysqli_stmt_bind_param($comando, 'sii',$entregador, $idcarrinho, $identrega);
+    $funcionou = mysqli_stmt_execute($comando);
+    mysqli_stmt_close($comando);
+    
+    return $funcionou;
+}
+
+function pesquisarEntregaId($conexao, $identrega) {
+    $sql = "SELECT * FROM entrega WHERE identrega = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_bind_param($comando, 'i', $identrega);
+
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+
+    $entrega = mysqli_fetch_assoc($resultado);
+
+    mysqli_stmt_close($comando);
+    return $entrega;
+}
+
+function deletarEntrega($conexao, $identrega) {
+    $sql = "DELETE FROM entrega WHERE identrega = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'i', $identrega);
+
+    $funcionou = mysqli_stmt_execute($comando);
+    mysqli_stmt_close($comando);
+    
+    return $funcionou;
+}
+
 //izabella
 
 // ADICIONAR VALOR PAGO E VALOR ENTREGA NO CARRINHO pronto
