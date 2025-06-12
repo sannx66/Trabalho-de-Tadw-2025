22cf7ac47ec7b4f1387dee882df947ef10c6dd54
iasminzera <iasminnymendes@gmail.com>
2025-06-12 10:24:23 -0300

    fiz o deletar e o listar login

--- tadw_e_dps_sanbenny/codigo/funcoes.php
+++ tadw_e_dps_sanbenny/codigo/funcoes.php
@@ -15,8 +15,34 @@ function salvarLogin($conexao, $nome, $email, $senha) {
 
 function verificarLogin($conexao, $email, $senha) {};
 function verificarLogado($conexao) {};
-function listarLogin($conexao) {};
-function deletarLogin($conexao, $idlogin) {};
+
+function listarLogin($conexao) {
+    $sql = "SELECT * FROM tb_login";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_execute($comando);
+    $resultados = mysqli_stmt_get_result($comando);
+    
+    $lista_login = [];
+    while ($login = mysqli_fetch_assoc($resultados)) {
+        $lista_login[] = $login;
+    }
+    mysqli_stmt_close($comando);
+
+    return $lista_login;
+};
+
+function deletarLogin($conexao, $idlogin) {
+    $sql = "DELETE FROM tb_login WHERE idlogin = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'i', $idlogin);
+
+    $funcionou = mysqli_stmt_execute($comando);
+    mysqli_stmt_close($comando);
+    
+    return $funcionou;
+};
 function pesquisarLogin($conexao, $idlogin) {};
 //toddy
 
