97ae38fcc6b3d05a6a297a4b83ac2e954da8b598
yomirkz <izaxxxsouza7777@gmail.com>
2025-08-25 13:52:32 -0300

    categorias

---
+++ tadw_e_dps_sanbenny/codigo/categoria.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><i>Categorias</i></h1> <br>
+
+    <a href="bolo.php">Bolos</a> <br> 
+    <a href="donuts.php">Donuts</a> <br> 
+    <a href="churros.php">Churros</a> <br> 
+    <a href="macarons.php">Macarons</a> <br> 
+    <a href="trufas.php">Trufas</a> <br> 
+    <a href="cafe.php">Cafés</a> <br> 
+    <a href="cha.php">Chás</a> <br> 
+    <a href="milkshake.php">Milshake</a> <br> 
+
+
+
+
+
+    
+
+</body>
+</html>
