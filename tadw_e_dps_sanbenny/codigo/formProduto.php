f4ed47ae8ebb174f46a073c2643e707837c4babd
sannx66 <sandymirellealves@gmail.com>
2025-09-22 13:27:19 -0300

    formproduto

--- tadw_e_dps_sanbenny/codigo/formProduto.php
+++ tadw_e_dps_sanbenny/codigo/formProduto.php
@@ -38,20 +38,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastro de Produtos</title>
 </head>
 <body>
-    <h1>Cadastro de Produto</h1>
+    <h1>Cadastro de Produtos</h1>
     <form action="salvarProduto.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
+        
         Foto: <br>
         <input type="file" name="foto"> <br><br>
-<!-- precisa terminar isso,n ta pronto -->
+
         Nome: <br>
         <input type="text" name="nome" value="<?php echo $nome; ?>"> <br><br>
+        
         Disponivel: <br>
-        <input type="text" name="cpf" value="<?php echo $cpf; ?>"> <br><br>
-        Endereço: <br>
-        <input type="text" name="endereco" value="<?php echo $endereco; ?>"> <br><br>
+        <input type="text" name="disponivel" value="<?php echo $disponivel; ?>"> <br><br>
+        
+        Tipo: <br>
+        <input type="text" name="tipo" value="<?php echo $tipo; ?>"> <br><br>
+        
+        Ingredientes: <br>
+        <input type="text" name="ingredientes" value="<?php echo $ingredientes; ?>"> <br><br>
+        
+        Valor_un: <br>
+        <input type="number" name="valor_un" value="<?php echo $valor_un; ?>"> <br><br>
+        
+        Observacoes: <br>
+        <input type="text" name="observacoes" value="<?php echo $observacoes; ?>"> <br><br>
 
         <input type="submit" value="<?php echo $botao; ?>">
     </form>
