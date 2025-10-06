e6e9cdae2582091fb4e20bc4ff51209ed1b2615d
iasminzera <iasminnymendes@gmail.com>
2025-10-06 13:54:52 -0300

    aaa

--- tadw_e_dps_sanbenny/codigo/index.php
+++ tadw_e_dps_sanbenny/codigo/index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="estilo.css" href="estilo.css">
     <title>Document</title>
 </head>
 <body>
 
     <h1>Doceria</h1>
- <h3>Le Douce Amoux</h3>
+    <h3>Le Douce Amoux</h3>
 
         <a href="cadastrarCliente.php">Cadastrar</a> <br><br>
         <a href="formCliente.php">Entrar</a> <br><br>
