6849c1d71ab0c0c72a829b326fb2727f04b47a1b
iasminzera <iasminnymendes@gmail.com>
2025-10-06 14:01:31 -0300

    fundo pronto graças a Deus

--- tadw_e_dps_sanbenny/codigo/index.php
+++ tadw_e_dps_sanbenny/codigo/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="estilo.css" href="estilo.css">
+    <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
 <body>
