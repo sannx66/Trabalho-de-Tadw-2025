99bfd79b3696c7df6f23d3511245562eea661aa5
iasminzera <iasminnymendes@gmail.com>
2025-10-09 10:46:13 -0300

    DEU CERTOOOO

--- tadw_e_dps_sanbenny/codigo/index.php
+++ tadw_e_dps_sanbenny/codigo/index.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
 <body>
 
-    <h1>Doceria</h1>
-    <h3>Le Douce Amoux</h3>
+<div class="cadastro-container">
+  <h2><i><b>Doceria</b></i></h2>
+</div>
 
+    <h3>Le Douce Amoux</h3>
+    
         <a href="cadastrarCliente.php">Cadastrar</a> <br><br>
         <a href="formCliente.php">Entrar</a> <br><br>
 
