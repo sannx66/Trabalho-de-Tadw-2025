92cd419a23b5e1220c1c78efd4674e271fe516cd
yomirkz <izaxxxsouza7777@gmail.com>
2025-10-09 10:10:33 -0300

    adicionando inserts e mudando as senhas

--- tadw_e_dps_sanbenny/banco.sql
+++ tadw_e_dps_sanbenny/banco.sql
@@ -128,3 +128,25 @@ VALUES
 ('fotos/teste.png', '10', 'Bolo', 'Bolo de chocolate amargo', 'Massa de chocolate amargo, trufado de chocolate branco, chocolate 100% cacau', 15.00, 'Intenso e sofisticado'),
 
 ('fotos/teste.png', '11', 'Bolo', 'Trufado de cereja', 'Massa de chocolate, mousse de chocolate com cereja, trufado de chocolate ao leite', 15.00, 'Com toque frutado e doce');
+
+
+INSERT INTO tb_cliente (email, senha, nome, telefone, endereco, status, tipo) 
+VALUES 
+  ('Todyy@gmail.com', SHA2('todyy', 256), 'Iasmin', '123456789', 'rua 2314', 'd', 'c'),
+  ('lucas@gmail.com', SHA2('lucas', 256), 'Lucas Silva', '987654321', 'av. Paulista, 1500', 'd', 'c'),
+  ('jose@example.com', SHA2('jose', 256), 'José Pereira', '1122334455', 'rua 4567', 'd', 'c'),
+  ('mariana@hotmail.com', SHA2('mariana', 256), 'Mariana Costa', '9988776655', 'rua das Flores, 1000', 'd', 'c'),
+  ('bruna@outlook.com', SHA2('bruna', 256), 'Bruna Almeida', '6677889900', 'av. Rio Branco, 200', 'd', 'c'),
+  ('fabio@site.com', SHA2('fabio', 256), 'Fábio Costa', '5556667777', 'rua Jardim, 32', 'd', 'c'),
+  ('camila@dominio.com', SHA2('camila', 256), 'Camila Souza', '4445556666', 'rua do Sol, 850', 'd', 'c'),
+  ('pedro@exemplo.com', SHA2('pedro', 256), 'Pedro Lima', '3334445555', 'av. Central, 50', 'd', 'c'),
+  ('ana@teste.com', SHA2('ana', 256), 'Ana Oliveira', '2223334444', 'rua das Palmeiras, 22', 'd', 'c'),
+  ('roberto@company.com', SHA2('roberto', 256), 'Roberto Martins', '1212121212', 'rua do Parque, 123', 'd', 'c'),
+  ('larissa@webmail.com', SHA2('larissa', 256), 'Larissa Santos', '1987654321', 'av. Brasil, 750', 'd', 'c'),
+  ('andrea@admin.com', SHA2('andrea', 256), 'Andrea Silva', '6667778888', 'rua dos Gestores, 11', 'd', 'g'),
+  ('rodrigo@manager.com', SHA2('rodrigo', 256), 'Rodrigo Carvalho', '9998887777', 'av. Empresarial, 40', 'd', 'g'),
+  ('patricia@corporate.com', SHA2('patricia', 256), 'Patricia Gomes', '5557779999', 'rua Executiva, 78', 'd', 'g'),
+  ('alex@adminsite.com', SHA2('alex', 256), 'Alex Ferreira', '4448887777', 'av. Liderança, 50', 'd', 'g'),
+  ('toddy@gmail.com', SHA2('toddy', 256), 'Iasminny', '4228887777', 'av. Onçinha, 59', 'd', 'g'),
+  ('sandy@gmail.com', SHA2('sandy', 256), 'Sandy', '444845377', 'av Barracão, 50', 'd', 'g'),
+  ('izabella@gmail.com', SHA2('izabella', 256), 'Izabella', '4446687777', 'rua itiopo, 50', 'd', 'g');
