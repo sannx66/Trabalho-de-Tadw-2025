f903e7871d1ac12f3c3a7025ced6c76d4884ffb6
iasminzera <iasminnymendes@gmail.com>
2025-10-16 10:55:22 -0300

    FINALMENTEEEE

--- tadw_e_dps_sanbenny/codigo/index.php
+++ tadw_e_dps_sanbenny/codigo/index.php
@@ -8,18 +8,29 @@
     <title>Document</title>
 </head>
 <body>
-
-<div class="cadastro-container">
-  <h1><b>Doceria</b></h1>
-</div>
-
-<div class="ledoux-container">
-    <h3>Le Doux Amour</h3>
-</div>
+  <div id="pai2">
+    <img src="./fotos/logo_diego.png" alt="logo_doceria" style="
+    width: 500px;
+    padding: 100px;
+">
+  </div>
+  <div id="pai">
+    <div class="cadastro-container">
+      <h1><b>Doceria</b></h1>
+    </div>
     
-        <a href="cadastrarCliente.php">Cadastrar</a> <br><br>
-        <a href="formCliente.php">Entrar</a> <br><br>
-
+    <div class="ledoux-container">
+        <h3>Le Doux Amour</h3>
+    </div>
+        
+    <div class="botaocadastrar-container">
+            <b><i><a href="cadastrarCliente.php">Cadastrar</a></i></b> <br><br>
+    </div>
+    
+    <div class="botaoform-container">
+            <b><i><a href="formCliente.php">Entrar</a></i></b> <br><br>
+    </div>
+  </div>
     
 
 </body>
