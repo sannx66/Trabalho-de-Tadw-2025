5020687913e45e9bb059dea84e6dc38d3edd45b6
yomirkz <izaxxxsouza7777@gmail.com>
2025-11-03 16:50:10 -0300

    tentando arrumar retirada.php

--- tadw_e_dps_sanbenny/codigo/retirada.php
+++ tadw_e_dps_sanbenny/codigo/retirada.php
@@ -1,18 +1,38 @@
 <?php
-    require_once "./verificarlogado.php";
-
+require_once "verificarlogado.php";
+require_once "conexao.php";
+require_once "funcoes.php";
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Retirada</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-    Prontinhooo!!! Só aguardar o seu pedido!! <br>
-    Obrigado pela preferência! <br>
-    Assim que tiver pronto te avisaremos!! 
+    <div id="retirada-page">
+        <h1 id="retirada-title">Estamos te esperando</h1>
+
+        <div class="card" id="retirada-card">
+            <img src="fotos/castiel.webp" id="retirada-castiel" alt="Castiel">
+            <form action="confirmar_retirada.php" method="post" id="retirada-form">
+                <label for="nome">Nome:</label><br>
+                <input type="text" name="nome" id="nome" placeholder="Seu nome" required><br><br>
+
+                <label for="telefone">Telefone:</label><br>
+                <input type="text" name="telefone" id="telefone" placeholder="(xx) xxxx-xxxx" required><br><br>
+
+                <label for="mensagem">Mensagem (opcional):</label><br>
+                <input type="text" name="mensagem" id="mensagem" placeholder="Observações"><br><br>
+
+                <input type="submit" value="Confirmar Retirada" id="retirada-submit">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
+
+
